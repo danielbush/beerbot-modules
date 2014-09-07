083bfbb924086c018c93b108a4d0b3e0800cd057
Daniel Bush <dlb.id.au@gmail.com>
2014-09-08 02:11:51 +1000

    Added Reminder module.

---
+++ Reminder/reminder.rb
@@ -0,0 +1,201 @@
+# The files in this directory are part of BeerBot, a a ruby irc bot library.
+# Copyright (C) 2013,2014 Daniel Bush
+# This program is distributed under the terms of the GNU
+# General Public License.  A copy of the license should be
+# enclosed with this project in the file LICENSE.  If not
+# see <http://www.gnu.org/licenses/>.
+
+require 'beerbot'
+require_relative '../utils/more'
+require 'json'
+
+
+module BeerBot; module Modules; end; end
+
+module BeerBot::Modules::Reminder
+
+  CUtils    = BeerBot::Scheduler::CronR::Utils
+  Job       = BeerBot::Scheduler::CronR::CronJob
+  Utils     = ::BeerBot::Modules::Utils
+  More      = Utils::More      
+
+  def self.scheduler
+    @scheduler
+  end
+
+  def self.help arr=[]
+    [
+      "Do: ,at minute hour dom month dow <message>",
+      "    one-off reminder",
+      "Do: ,cron minute hour dom month dow <message>",
+      "    repeating reminder",
+      "If you run this in a channel, the channel will own and get the reminder",
+      "If you message the bot directly, you will own and get the reminder",
+    ]
+  end
+
+  def self.init config
+    @scheduler = BeerBot::Scheduler.instance
+    @config = config
+    @more = More.new(5)
+    # Load what we saved last time:
+    self.load
+  end
+
+  def self.cmd msg, from:nil, to:nil, me:false, config:nil
+    replyto = me ? from : to
+    parsed = self.parse_args(msg)
+    unless parsed then
+      return nil
+    end
+    cmd, *params = parsed
+
+    case cmd
+
+    when 'atmore', 'cronmore'
+      if @more.more?(replyto) then
+        @more.more(replyto)
+      else
+        nil
+      end
+
+    when 'atls', 'cronls'
+      jobs = @scheduler.select {|job|
+        job[:reminder] &&
+        job[:owner] == replyto
+      }.map {|job|
+        {to: replyto, msg: "#{job[:id]} (#{job[:owner]}) #{job[:msg]}"}
+      }
+      @more[replyto] = jobs
+      @more.more(replyto)
+
+    when 'at', 'cron'
+      msg, cronr_params = params
+      msg = [to: replyto, msg: msg]
+      job = Job.new(self.new_id, *cronr_params) {
+        msg
+      }
+      # Set ownership, which will determine who can delete.
+      # If !me, then 'to' is a channel, and anyone on channel can delete
+      job[:owner] = replyto
+      job[:once] = true if cmd == 'at'
+      job[:msg] = msg  # so we can persist
+      job[:reminder] = true
+      @scheduler.push(job)
+      self.save
+      [to: replyto, msg: "Created job #{job[:id]}, #{from}"]
+
+    when 'atrm', 'cronrm'
+      id,_ = params
+      job = @scheduler.find{|job| job[:id] == id }
+      if job then
+        # Check ownership
+        if job.has_key?(:owner) && (job[:owner] != replyto) then
+          [to: replyto, msg: "I'm afraid you don't have permission, #{from}"]
+        else
+          @scheduler.reject!{|job| job[:id] == id }
+          [to: replyto, msg: "Deleted job #{id}, #{from}"]
+        end
+      else
+        [to: replyto, msg: "Can't find job #{id}, #{from}"]
+      end
+
+    end
+
+  #rescue => e
+    #[to: replyto, msg: e.message]
+
+  end
+
+  def self.hear msg, config:nil, from:nil, to:nil, me:false
+  end
+
+  def self.action action, config:nil, from:nil, to:nil, me:false
+  end
+
+  def self.savepath
+    File.join(@config.module_data('Reminder'), 'savefile.json')
+  end
+
+  # Persist to a file store.
+  def self.save
+    File.write(self.savepath, @scheduler.to_json)
+  end
+
+  # Load from file store.
+
+  def self.load
+    jobs = JSON.load(File.read(self.savepath))
+    puts "Loading reminders from #{self.savepath}, #{jobs.size} reminders found"
+    @scheduler.reject!{true}
+    jobs.each {|j|
+      # Eek... convert keys to syms again
+      msg = j['msg'].map {|h| {to: h['to'], msg: h['msg']}}
+      job = Job.new(j['id'], j['minute'], j['hour'], j['day'], j['month'], j['dow']) {
+        msg
+      }
+      job[:reminder] = true
+      job[:owner] = j['owner']
+      job[:once] = j['once']
+      job[:msg] = msg
+      @scheduler.push(job)
+    }
+    jobs
+  rescue => e
+    nil
+  end
+
+  # Generate a new unique id.
+
+  def self.new_id
+    max = @scheduler
+      .select {|job| job[:reminder] }
+      .map {|job| job[:id] }
+      .max
+    (max ? max : 0) + 1
+  end
+
+  # Parse the bit after the "," (command prefix).
+  #
+  # @return Array ["at"|"cron", msg, [CronR parameters]]
+  #
+  # Raises exception!
+  # If 'str' doesn't start with 'at' or 'cmd', then return nil
+  # (ignore).
+  #
+  # We want to use CronR.
+  # CronR uses just booleans, Fixnums or arrays of Fixnums.
+  # true  => cron: *
+  # i     => cron: a
+  # [...] => cron: a,b,c OR a-b,c-d or */a or a-b/k 
+  # 
+
+  def self.parse_args str
+    cmd, *args = str.split(/\s+/, 7)
+    case cmd
+    when "atls", "cronls", "atmore", "cronmore"
+      [cmd]
+
+    when "atrm", "cronrm"
+      id,_ = args
+      id = id.to_i
+      [cmd, id]
+
+    when "at", "cron"
+      min, hour, dom, month, dow, msg = args
+      # This guarantees we got 5 cron params + message:
+      if msg.nil? then
+        raise "Couldn't detect a message"
+      end
+      # Parse remainder as cron parameters for use with CronR:
+      # This could throw an error.
+      params = [min, hour, dom, month, dow].map {|m|
+        CUtils.parse_param(m)
+      }
+      [cmd, msg, params]
+
+    end
+  end
+
+
+end
---
+++ spec/Reminder/reminder_spec.rb
@@ -0,0 +1,155 @@
+
+require 'pp'
+require_relative '../../Reminder/reminder'
+
+Reminder = BeerBot::Modules::Reminder
+
+describe "Reminder module", :reminder => true do
+
+  describe "parsing inputs from users" do
+    it "should parse cron args" do
+      Reminder.parse_args('at 1-3 17 4-11/2 * * foo')
+        .should == ['at', 'foo', [[1, 2, 3], [17], [4, 6, 8, 10], true, true]]
+    end
+  end
+
+  describe "adding jobs" do
+
+    before(:each) {
+      config = BeerBot::Config.new(datadir: '/tmp', moduledir: '/tmp')
+      Reminder.init(config)
+      Reminder.scheduler.reject!{true}
+    }
+
+    it "should add at-commands" do
+      r = Reminder.cmd("at * * * * * test message", from: 'from1', to: 'to1')
+      r.first[:msg].should == 'Created job 1, from1'
+      Reminder.scheduler.size.should == 1
+      Reminder.scheduler.first[:id].should == 1
+      Reminder.scheduler.first[:once].should == true
+      Reminder.scheduler.first.run.should == [to: 'to1', msg: 'test message']
+    end
+
+    it "should add cron-commands" do
+      r = Reminder.cmd("cron * * * * * test message", from: 'from1', to: 'to1')
+      r.first[:msg].should == 'Created job 1, from1'
+      Reminder.scheduler.size.should == 1
+      Reminder.scheduler.first[:id].should == 1
+      Reminder.scheduler.first[:once].should == nil
+      Reminder.scheduler.first.run.should == [to: 'to1', msg: 'test message']
+    end
+
+    it "should set the owner property on the cronjob", :owner => true do
+      r = Reminder.cmd("at * * * * * test message", from: 'from1', to: 'to1', me: false)
+      Reminder.scheduler[0][:owner].should == 'to1' # to1 = channel
+      r = Reminder.cmd("at * * * * * test message", from: 'from1', to: 'to1', me: true)
+      Reminder.scheduler[1][:owner].should == 'from1' # personal cron job
+    end
+
+    it "should store the message (so we can persist it)" do
+      r = Reminder.cmd("at * * * * * test message", from: 'from1', to: 'to1', me: false)
+      Reminder.scheduler.first[:msg].should == [to: 'to1', msg: 'test message']
+    end
+
+  end
+
+  describe "removing jobs" do
+
+    before(:each) {
+      Reminder.scheduler.reject!{true}
+      Reminder.scheduler.push(Reminder::Job.new(1))
+      Reminder.scheduler.push(Reminder::Job.new(2))
+      # Create some jobs that have ownership set:
+      j = Reminder::Job.new(3)
+      j[:owner] = '#some-channel'
+      Reminder.scheduler.push(j)
+      j = Reminder::Job.new(4)
+      j[:owner] = 'some-user'
+      Reminder.scheduler.push(j)
+    }
+
+    it "should remove by id" do
+      r = Reminder.cmd("atrm 1", from: 'from1', to: 'to1')
+      Reminder.scheduler.map {|job| job[:id]}.sort.should == [2, 3, 4]
+    end
+
+    it "should respond to cronrm in the same way" do
+      r = Reminder.cmd("cronrm 1", from: 'from1', to: 'to1')
+      Reminder.scheduler.map {|job| job[:id]}.sort.should == [2, 3, 4]
+    end
+
+    it "should remove if ownership matches" do
+      r = Reminder.cmd("atrm 3", from: 'from1', to: '#some-channel')
+      Reminder.scheduler.map {|job| job[:id]}.sort.should == [1, 2, 4]
+      expect(r.first[:msg]).to match(/Deleted job 3/)
+    end
+
+    it "should refuse to remove if ownership doesn't match" do
+      r = Reminder.cmd("atrm 3", from: 'from1', to: '#not-some-channel')
+      Reminder.scheduler.map {|job| job[:id]}.sort.should == [1, 2, 3, 4]
+      expect(r.first[:msg]).to match(/I'm afraid you don't have permission/)
+    end
+
+  end
+
+  describe "listing jobs" do
+
+    # We should only list jobs on the channel
+    # or for the user when pm'ing the bot.
+
+    before(:each) {
+      Reminder.scheduler.reject!{true}
+      Reminder.scheduler.push(job=Reminder::Job.new(1,1,2,3,4,5){[to: '#some-channel', msg: 'msg1']})
+      job[:msg] = [to: '#some-channel', msg: 'msg1']
+      job[:owner] = '#some-channel'
+      job[:reminder] = true
+      Reminder.scheduler.push(job=Reminder::Job.new(1,1,2,3,4,5){[to: '#some-channel', msg: 'msg3']})
+      job[:msg] = [to: '#some-channel', msg: 'msg3']
+      job[:owner] = '#some-channel'
+      job[:reminder] = true
+      Reminder.scheduler.push(job=Reminder::Job.new(2,true,7,8,9,10){[to: 'someone', msg: 'msg2']})
+      job[:msg] = [to: 'someone', msg: 'msg2']
+      job[:owner] = 'someone'
+      job[:reminder] = true
+    }
+
+    it "should list jobs matching :owner" do
+      r = Reminder.cmd("atls", from: 'from1', to: '#some-channel')
+      expect(r.size).to be(2)
+      r = Reminder.cmd("atls", from: 'someone', to: 'beerbot', me: true)
+      expect(r.size).to be(1)
+    end
+
+  end
+
+  describe "persisting jobs" do
+
+    before(:each) {
+      config = BeerBot::Config.new(datadir: '/tmp', moduledir: '/tmp')
+      Reminder.init(config)
+      Reminder.scheduler.reject!{true}
+      Reminder.scheduler.push(job=Reminder::Job.new(1,1,2,3,4,5){[to: 'to1', msg: 'msg1']})
+      job[:msg] = [to: 'to1', msg: 'msg1']
+      Reminder.scheduler.push(job=Reminder::Job.new(2,true,7,8,9,10){[to: 'to2', msg: 'msg2']})
+      job[:msg] = [to: 'to2', msg: 'msg2']
+    }
+
+    it "can save the scheduler to a json file and reload it" do
+      Reminder.save
+      # Let's wipe the Scheduler...
+      Reminder.scheduler.reject!{true}
+      expect(Reminder.scheduler.empty?).to be(true)
+      jobs = Reminder.load
+      expect(Reminder.scheduler[0][:id]).to be(1)
+      expect(Reminder.scheduler[0].run).to match_array([to: 'to1', msg: 'msg1'])
+      expect(Reminder.scheduler[1][:id]).to be(2)
+      expect(Reminder.scheduler[1].run).to match_array([to: 'to2', msg: 'msg2'])
+    end
+
+  end
+
+  describe "running jobs" do
+    it "should run and delete jobs flagged with :once"  # this is probably automatic
+  end
+
+end
